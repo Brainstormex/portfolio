f41bc596d4101ef94633d27ca39b54935c5d5f19
Brainstormex <uzersayed9@gmail.com>
2024-04-24 01:24:02 +0530

    header

---
+++ src/lib/data.ts
@@ -0,0 +1,39 @@
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  
+] as const;
+
+export const projectsData = [
+  
+] as const;
+
+export const skillsData = [
+  
+] as const;
